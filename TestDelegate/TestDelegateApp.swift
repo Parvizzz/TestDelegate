cce559bd16509516c8c61aa5f798cb3fc24c5eea
Parvizzz <pavrik_95@mail.ru>
2025-02-07 08:42:00 +0500

    Initial Commit

---
+++ TestDelegate/TestDelegateApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestDelegateApp.swift
+//  TestDelegate
+//
+//  Created by Parviz on 07/02/25.
+//
+
+import SwiftUI
+
+@main
+struct TestDelegateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
